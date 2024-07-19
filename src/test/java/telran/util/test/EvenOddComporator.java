47b61f207a3911b9e7bdd30e560aa9a0132daaf7
Nahum Alon <nochum.shepsl@gmail.com>
2024-07-19 13:34:05 +0300

    In class EvenOddComporator realized method compare

--- src/test/java/telran/util/test/EvenOddComporator.java
+++ src/test/java/telran/util/test/EvenOddComporator.java
@@ -6,8 +6,16 @@ public class EvenOddComporator implements Comparator<Integer> {
 
     @Override
     public int compare(Integer arg0, Integer arg1) {
-        // TODO
-        return -1;
+        int res = 0;
+        if (arg0 % 2 == 0 && arg1 % 2 == 0) {
+            res = arg0 - arg1;
+        } else if (arg0 % 2 != 0 && arg1 % 2 != 0) {
+            res = arg1 - arg0;
+        } else if (arg0 % 2 == 0 && arg1 % 2 != 0) {
+            res = -1;
+        } else {
+            res = 1;
+        }
+        return res;
     }
-
 }
